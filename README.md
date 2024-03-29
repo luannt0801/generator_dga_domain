a29667014b43a86072d966b29e04666cc69c181a
luannt0801 <ntl080102@gmail.com>
2024-03-29 22:25:28 +0700

    updata dirichlet

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ git push -u origin main
 ### To generator domain dga:
 1. number_of_file
 2. number_of_sample: sample in each file
+
+```
+python .\Script.py --type normal --n 100 --num_file 1 --name_client jetson --num_client 1
+python .\Script.py --type dirichlet --num_file 1 --data_dirichlet 1000 --num_client 1 --name_client luan
+```
--- Script.py
+++ Script.py
@@ -149,17 +149,9 @@ def dirichlet(num_class,num_clients, num_data):
     alpha = np.ones(num_class)
 
     data_for_clients = np.random.dirichlet(alpha, size=num_clients)
-    print(f"data for client : " , data_for_clients)
+    # print(f"data for client : " , data_for_clients)
 
     client_ratios = (num_data * data_for_clients / np.sum(data_for_clients, axis=1)[:, np.newaxis]).astype(int)
-
-    # set_dirichlet_client = []
-
-    # # In ra tỷ lệ dữ liệu cho từng client
-    # for i, client_ratio in enumerate(client_ratios, 1):
-    #     # print(f"Client {i}: {client_ratio}")
-    #     set_dirichlet_client.append(client_ratio)        
-    # return set_dirichlet_client
     return client_ratios
 
 def generate_samples_for_clients(number_of_sample, number_file, dga_labels, type_for_benign, machine):
@@ -208,27 +200,71 @@ def generate_samples_for_clients(number_of_sample, number_file, dga_labels, type
     
 if __name__ == "__main__":
 
-    # normal confict
-    num_of_sample = 2500 # sample in a file
-    num_file = 1  # number of file
-    num_clients = 5 # machone
+    parser = argparse.ArgumentParser(description='Test Algorithms.')
+    # general settings
+    parser.add_argument('--type', default='normal', type=str, help='choose type to generate scripts')
+    parser.add_argument('--n', default='100', type=int, help='input number of sample for each file')
+    parser.add_argument('--num_file', default='1', type=int, help='input number of file in each type data')
+    parser.add_argument('--name_client', default='jetson', type=str, help='choose jetson or machine')
+    parser.add_argument('--data_dirichlet', default='1000', type=int, help='data for dirichlet')
+    parser.add_argument('--num_client', default=5,type= int , help='so luong client')
+    args = parser.parse_args()
+    # # normal confict
+    # num_of_sample = 5000 # sample in a file
+    # num_file = 1  # number of file
+    # num_clients = 5 # machone
     type_for_benign = 1
-    machine = 'machine_'
-    dga_labels = ['corebot', 'dircrypt', 'dnschanger', 'fobber', 'necurs', 'newgoz', 'qakbot', 'rambo', 'ramnit', 'banjori', 'benign']
-
-    # normal
-    # for i in range (1, num_clients + 1):
-    #     client_folder_name = f"{machine}_{i}"
-    #     generate_samples_for_clients(num_of_sample, num_file, dga_labels, type_for_benign, client_folder_name)
+    # machine = 'machine'
 
     # # new setup
     num_class = 10
-    num_data = 1000
+    # num_data = 1000
+    sum = 0
 
-    dirichlet_clients = dirichlet(num_class, num_clients, num_data)
-    print(dirichlet_clients)
-    print(dirichlet_clients[0][1])
-    # for i, client in enumerate(dirichlet_clients):
-    #     machine = f"{machine_prefix}{i}"
-    #     generate_samples_for_clients(number_of_sample, number_file, dga_labels, machine)WWW
-    for 
+    num_of_sample = args.n
+    num_file = args.num_file 
+    num_clients = args.num_client
+    name_client = args.name_client
+    num_data = args.data_dirichlet
+    type = args.type
+    dga_labels = ['corebot', 'dircrypt', 'dnschanger', 'fobber', 'necurs', 'newgoz', 'qakbot', 'rambo', 'ramnit', 'banjori', 'benign']
+
+    if type == 'normal':
+        for i in range (1, num_clients + 1):
+            client_folder_name = f"{name_client}_{i}"
+            generate_samples_for_clients(num_of_sample, num_file, dga_labels, type_for_benign, client_folder_name)
+
+    elif type == 'dirichlet':
+        dirichlet_clients = dirichlet(num_class, num_clients, num_data)
+        print(dirichlet_clients)
+        # print(dirichlet_clients[0][1])
+        # for i, client in enumerate(dirichlet_clients):
+        #     machine = f"{machine_prefix}{i}"
+        #     generate_samples_for_clients(number_of_sample, number_file, dga_labels, machine)WWW
+        for i in range(0,num_clients):
+            # print(f" client {i}: {dirichlet_clients[i]}")
+            machine = f"{name_client}_{i+1}"
+            for j in range(len(dirichlet_clients[i])):
+                x = dirichlet_clients[i][j]
+                sum += x
+                if j == 0:
+                    corebot(x, num_file, 'corebot', machine)
+                elif j == 1:
+                    dircypt(x, num_file, 'dircypt', machine)
+                elif j == 2:
+                    dnschanger(x, num_file, 'dnschanger', machine)
+                elif j == 3:
+                    fobber(x, num_file, 'fobber', machine)
+                elif j == 4:
+                    necurs(x, num_file, 'necurs', machine)
+                elif j == 5:
+                    newgoz(x, num_file, 'newgoz', machine)
+                elif j == 6:
+                    qakbot(x, num_file, 'qakbot', machine)
+                elif j == 7:
+                    rambo(x, num_file, 'rambo', machine)
+                elif j == 8:
+                    ramnit(x, num_file, 'ramnit', machine)
+                elif j == 9:
+                    banjori(x, num_file, 'banjori', machine)
+            benign(int(sum/10), num_file, type_for_benign, machine)
