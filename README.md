5d3db5faf83c53941149a31ed8a02b23946ef37f
luannt0801 <ntl080102@gmail.com>
2025-01-16 23:04:47 +0700

    gen DGA domain data

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-```
+<!-- ```
 git remote add origin https://github.com/luannt0801/generator_dga_domain.git
 git branch -M main
 git push -u origin main
-```
+``` -->
 
 ### To generator domain dga:
 1. number_of_file
@@ -15,19 +15,7 @@ git push -u origin main
 #### --data_dirichlet | total sample dga in each client
 #### --name_client | jetson | machine
 
-### normal
-```
-python .\Script.py --type normal --n 100 --num_file 1 --num_client 1 --name_client jetson
-```
-### dirichlet
-```
-python .\Script.py --type dirichlet --data_dirichlet 10000 --num_file 1 --num_client 1 --name_client jetson
-```
-### non-iid
-```
-python .\Script.py --type non_iid --n 100 --num_file 1 --num_client 1 --name_client luan
-```
-### dirichlet_client
-```
-python .\Script.py --type dirichlet_client --num_file 1 --num_client 10 --name_client machine
+### Running
+```python
+python Script.py --n 100000 --num_file 1 --name_client dai --num_client 1
 ```
--- Script.py
+++ Script.py
@@ -2,270 +2,90 @@ import random
 import numpy as np
 import argparse
 
-def corebot(number_of_sample, number_file, type_dga, machine):
-    type_dga = 'corebot'
-    print(f'mkdir {machine}\\{type_dga}')
-    for i in range (1,number_file+1):
-        n = random.randint(432, 9348)
-        n = random.randint(1,28)
-        m = random.randint(1, 12)
-        y = random.randint(1000, 5000)
-        print(f'python .\corebot.py -s {n} -d "{y}-{m}-{n}" -n {number_of_sample} --output_file {machine}\\corebot\\corebot_{number_of_sample}_{i}.txt')
-
-def dircypt(number_of_sample, number_file, type_dga, machine):
-    type_dga = 'dircrypt'
-    print(f'mkdir {machine}\\{type_dga}')
-    for i in range(1,number_file+1):
-        n = random.randint(432, 9348)
-        print(f'python dircrypt.py {n} -n {number_of_sample} --output_file {machine}\\dircrypt\\dircrypt_{number_of_sample}_{i}.txt')
-
-def dnschanger(number_of_sample, number_file, type_dga, machine):
-    type_dga = 'dnschanger'
-    print(f'mkdir {machine}\\{type_dga}')
-    for i in range (1, number_file+1):
-        n = random.randint(432, 9348)
-        print(f'python dnschanger.py {n} -n {number_of_sample} --output_file {machine}\\dnschanger\\dnschanger_{number_of_sample}_{i}.txt')
-
-def fobber(number_of_sample, number_file, type_dga, machine):
-    type_dga = 'fobber'
-    print(f'mkdir {machine}\\{type_dga}')
-    for i in range(1,number_file+1):
-        n = random.randint(1, 2)
-        print(f'python fobber.py {n} -n {number_of_sample} --output_file {machine}\\fobber\\fobber_{number_of_sample}_{i}.txt')
-
-def newgoz(number_of_sample, number_file, type_dga, machine):
-    type_dga = 'newgoz'
-    print(f'mkdir {machine}\\{type_dga}')
-    for i in range (1,number_file+1):
-        print(f'python newgoz.py -n {number_of_sample} --output_file {machine}\\newgoz\\newgoz_{number_of_sample}_{i}.txt')
-
-def necurs(number_of_sample, number_file, type_dga, machine):
-    type_dga = 'necurs'
-    print(f'mkdir {machine}\\{type_dga}')
-    for i in range(1,number_file+1):
-        n = random.randint(1,31)
-        m = random.randint(1, 13)
-        y = random.randint(1000, 5000)
-        print(f'python necurs.py -n {number_of_sample} -d "{y}-{m}-{n}" --output_file {machine}\\necurs\\necurs_{number_of_sample}_{i}.txt')
-
-def ramnit(number_of_sample, number_file, type_dga, machine):
-    type_dga = 'ramnit'
-    print(f'mkdir {machine}\\{type_dga}')
-    for i in range (1,number_file+1):
-        n = random.randint(56, 230928)
-        tail_list = ['.com', '.net','.org']
-        tail = random.choice(tail_list)
-        print(f'python ramnit.py {n} -n {number_of_sample} -t {tail} --output_file {machine}\\ramnit\\ramnit_{number_of_sample}_{i}.txt')
-
-def rambo(number_of_sample, number_file, type_dga, machine):
-    type_dga = 'rambo'
-    print(f'mkdir {machine}\\{type_dga}')
-    for i in range(1,number_file+1):
-        print(f'python rambo.py -n {number_of_sample} --output_file {machine}\\rambo\\ramdo_{number_of_sample}_{i}.txt')
-        
-def qakbot(number_of_sample, number_file, type_dga, machine):
-    type_dga = 'qakbot'
-    print(f'mkdir {machine}\\{type_dga}')
-    for i in range (1,number_file+1):
-        n = random.randint(1,31)
-        m = random.randint(1, 13)
-        y = random.randint(1000, 5000)
-        x = random.randint(0,1)
-        print(f'python qakbot.py -n {number_of_sample} --date "{y}-{m}-{n}" --seed {x} --output_file {machine}\\qakbot\\qakbot_{number_of_sample}_{i}.txt')
-
-def banjori(number_of_sample, number_file, type_dga, machine):
-    type_dga = 'banjori'
-    print(f'mkdir {machine}\\{type_dga}')
-    for i in range(1,number_file+1):
-        print(f'python banjori.py -n {number_of_sample} --output_file {machine}\\banjori\\banjori_{number_of_sample}_{i}.txt')
-
-def benign(number_of_sample, number_file, type_for_benign, machine):
-    type_dga = 'benign'
-    for i in range (1,number_file+1):
-        if type_for_benign == 1:
-            print(f'python benign.py -n {number_of_sample*10} --type {type_for_benign} --output_file {machine}\\benign.txt')
-        if type_for_benign == 0:
-            line = 1000000/number_of_sample
-            print(f'python benign.py --type {type_for_benign} --start {line*i} --end {line*(i+1)} --output_file {machine}\\benign.txt')
-
-def print_all(number_of_sample, number_file, type_dga, type_for_benign, machine):
+def gen_scripts(number_of_sample, number_file, dga_types, machine):
     print(f"mkdir {machine}")
-    corebot(number_of_sample, number_file, type_dga, machine)
-    banjori(number_of_sample, number_file, type_dga, machine)
-    dircypt(number_of_sample, number_file, type_dga, machine)
-    dnschanger(number_of_sample, number_file, type_dga, machine)
-    fobber(number_of_sample, number_file, type_dga, machine)
-    necurs(number_of_sample, number_file, type_dga, machine)
-    newgoz(number_of_sample, number_file, type_dga, machine)
-    qakbot(number_of_sample, number_file, type_dga, machine)
-    rambo(number_of_sample, number_file, type_dga, machine)
-    ramnit(number_of_sample, number_file, type_dga, machine)
-    benign(number_of_sample, number_file, type_dga, type_for_benign, machine)
+    for type in dga_types:
+        if type == 'corebot':
+            print(f'mkdir {machine}\\{type}')
+            for i in range (1,number_file+1):
+                n = random.randint(432, 9348)
+                n = random.randint(1,28)
+                m = random.randint(1, 12)
+                y = random.randint(1000, 5000)
+                print(f'python .\corebot.py -s {n} -d "{y}-{m}-{n}" -n {number_of_sample} --output_file {machine}\\corebot\\corebot_{number_of_sample}_{i}.txt')
+        elif type == 'dircrypt':
+            print(f'mkdir {machine}\\{type}')
+            for i in range(1,number_file+1):
+                n = random.randint(432, 9348)
+                print(f'python dircrypt.py {n} -n {number_of_sample} --output_file {machine}\\dircrypt\\dircrypt_{number_of_sample}_{i}.txt')
 
-def dirichlet(num_class, num_clients, num_data):
-    np.random.seed(0)
-    alpha = np.ones(num_class)
-    data_for_clients = np.random.dirichlet(alpha, size=num_clients)
-    client_ratios = (num_data * data_for_clients / np.sum(data_for_clients, axis=1)[:, np.newaxis]).astype(int)
-    client_ratios += 1
-    return client_ratios
+        elif type == 'dnschanger':
+            print(f'mkdir {machine}\\{type}')
+            for i in range (1, number_file+1):
+                n = random.randint(432, 9348)
+                print(f'python dnschanger.py {n} -n {number_of_sample} --output_file {machine}\\dnschanger\\dnschanger_{number_of_sample}_{i}.txt')
 
-result = dirichlet(num_class=10, num_clients=50, num_data=100)
-print(result)
+        elif type == 'fobber':
+            print(f'mkdir {machine}\\{type}')
+            for i in range(1,number_file+1):
+                n = random.randint(1, 2)
+                print(f'python fobber.py {n} -n {number_of_sample} --output_file {machine}\\fobber\\fobber_{number_of_sample}_{i}.txt')
 
-def generate_samples_for_clients(number_of_sample, number_file, dga_labels, type_for_benign, machine):
-    for label in dga_labels:
-        if label == 'corebot':
-            corebot(number_of_sample, number_file, label, machine)
-        elif label == 'dircrypt':
-            dircypt(number_of_sample, number_file, label, machine)
-        elif label == 'dnschanger':
-            dnschanger(number_of_sample, number_file, label, machine)
-        elif label == 'fobber':
-            fobber(number_of_sample, number_file, label, machine)
-        elif label == 'necurs':
-            necurs(number_of_sample, number_file, label, machine)
-        elif label == 'newgoz':
-            newgoz(number_of_sample, number_file, label, machine)
-        elif label == 'qakbot':
-            qakbot(number_of_sample, number_file, label, machine)
-        elif label == 'rambo':
-            rambo(number_of_sample, number_file, label, machine)
-        elif label == 'ramnit':
-            ramnit(number_of_sample, number_file, label, machine)
-        elif label == 'banjori':
-            banjori(number_of_sample, number_file, label, machine)
-        elif label == 'benign':
-            benign(number_of_sample, number_file, type_for_benign, machine)
+        elif type == 'newgoz':
+            print(f'mkdir {machine}\\{type}')
+            for i in range (1,number_file+1):
+                print(f'python newgoz.py -n {number_of_sample} --output_file {machine}\\newgoz\\newgoz_{number_of_sample}_{i}.txt')
+
+        elif type == 'necurs':
+            print(f'mkdir {machine}\\{type}')
+            for i in range(1,number_file+1):
+                n = random.randint(1,31)
+                m = random.randint(1, 13)
+                y = random.randint(1000, 5000)
+                print(f'python necurs.py -n {number_of_sample} -d "{y}-{m}-{n}" --output_file {machine}\\necurs\\necurs_{number_of_sample}_{i}.txt')
+
+        elif type == 'ramnit':
+            print(f'mkdir {machine}\\{type}')
+            for i in range (1,number_file+1):
+                n = random.randint(56, 230928)
+                tail_list = ['.com', '.net','.org']
+                tail = random.choice(tail_list)
+                print(f'python ramnit.py {n} -n {number_of_sample} -t {tail} --output_file {machine}\\ramnit\\ramnit_{number_of_sample}_{i}.txt')
+
+        elif type == 'rambo':
+            print(f'mkdir {machine}\\{type}')
+            for i in range(1,number_file+1):
+                print(f'python rambo.py -n {number_of_sample} --output_file {machine}\\rambo\\ramdo_{number_of_sample}_{i}.txt')
+ 
+        elif type == 'qakbot':
+            print(f'mkdir {machine}\\{type}')
+            for i in range (1,number_file+1):
+                n = random.randint(1,31)
+                m = random.randint(1, 13)
+                y = random.randint(1000, 5000)
+                x = random.randint(0,1)
+                print(f'python qakbot.py -n {number_of_sample} --date "{y}-{m}-{n}" --seed {x} --output_file {machine}\\qakbot\\qakbot_{number_of_sample}_{i}.txt')
+
+        elif type == 'banjori':
+            print(f'mkdir {machine}\\{type}')
+            for i in range(1,number_file+1):
+                print(f'python banjori.py -n {number_of_sample} --output_file {machine}\\banjori\\banjori_{number_of_sample}_{i}.txt')
     
 if __name__ == "__main__":
     np.random.seed(0)
 
     parser = argparse.ArgumentParser(description='Test Algorithms.')
-
-    parser.add_argument('--type', default='normal', type=str, help='choose type to generate scripts')
     parser.add_argument('--n', default='100', type=int, help='input number of sample for each file')
     parser.add_argument('--num_file', default='1', type=int, help='input number of file in each type data')
     parser.add_argument('--name_client', default='jetson', type=str, help='choose jetson or machine')
-    parser.add_argument('--data_dirichlet', default='1000', type=int, help='data for dirichlet')
     parser.add_argument('--num_client', default=5,type= int , help='so luong client')
     args = parser.parse_args()
 
     type_for_benign = 1
 
-    num_class = 10
-    sum_dirichlet = 0
-
-    num_of_sample = args.n
-    num_file = args.num_file 
-    num_clients = args.num_client
-    name_client = args.name_client
-    num_data = args.data_dirichlet
-    type = args.type
     dga_labels = ['corebot', 'dircrypt', 'dnschanger', 'fobber', 'necurs', 'newgoz', 'qakbot', 'rambo', 'ramnit', 'banjori', 'benign']
 
-    if type == 'normal':
-        for i in range (1, num_clients + 1):
-            client_folder_name = f"{name_client}_{i}"
-            generate_samples_for_clients(num_of_sample, num_file, dga_labels, type_for_benign, client_folder_name)
-
-    elif type == 'dirichlet':
-        dirichlet_clients = dirichlet(num_class, num_clients, num_data)
-        print(dirichlet_clients)
-        # print(dirichlet_clients[0][1])
-        # for i, client in enumerate(dirichlet_clients):
-        #     machine = f"{machine_prefix}{i}"
-        #     generate_samples_for_clients(number_of_sample, number_file, dga_labels, machine)WWW
-        for i in range(0,num_clients):
-            # print(f" client {i}: {dirichlet_clients[i]}")
-            machine = f"{name_client}_{i+1}"
-            for j in range(len(dirichlet_clients[i])):
-                x = dirichlet_clients[i][j]
-                sum_dirichlet += x
-                if j == 0:
-                    corebot(x, num_file, 'corebot', machine)
-                elif j == 1:
-                    dircypt(x, num_file, 'dircypt', machine)
-                elif j == 2:
-                    dnschanger(x, num_file, 'dnschanger', machine)
-                elif j == 3:
-                    fobber(x, num_file, 'fobber', machine)
-                elif j == 4:
-                    necurs(x, num_file, 'necurs', machine)
-                elif j == 5:
-                    newgoz(x, num_file, 'newgoz', machine)
-                elif j == 6:
-                    qakbot(x, num_file, 'qakbot', machine)
-                elif j == 7:
-                    rambo(x, num_file, 'rambo', machine)
-                elif j == 8:
-                    ramnit(x, num_file, 'ramnit', machine)
-                elif j == 9:
-                    banjori(x, num_file, 'banjori', machine)
-            benign(int(sum/10), num_file, type_for_benign, machine)
-
-    # non iid num_sample, num client, num file, type_dga, machine
-    elif type == 'non_iid':
-        for i in range(0,num_clients):
-            # print(f" client {i}: {dirichlet_clients[i]}")
-            machine = f"{name_client}_{i+1}"
-            if i == 0:
-                corebot(num_of_sample, num_file, 'corebot', machine)
-                benign(int(num_of_sample/10), num_file, 1, machine)
-            if i == 1:
-                dircypt(num_of_sample, num_file, 'dircrypt', machine)
-                benign(int(num_of_sample/10), num_file, 1, machine)
-            if i == 2:
-                dnschanger(num_of_sample, num_file, 'dnschanger', machine)
-                benign(int(num_of_sample/10), num_file, 1, machine)
-            if i == 3:
-                fobber(num_of_sample, num_file, 'fobber', machine)
-                benign(int(num_of_sample/10), num_file, 1, machine)
-            if i == 4:
-                necurs(num_of_sample, num_file, 'necurs', machine)
-                benign(int(num_of_sample/10), num_file, 1, machine)
-            if i == 5:
-                newgoz(num_of_sample, num_file, 'newgoz', machine)
-                benign(int(num_of_sample/10), num_file, 1, machine)
-            if i == 6:
-                qakbot(num_of_sample, num_file, 'qakbot', machine)
-                benign(int(num_of_sample/10), num_file, 1, machine)
-            if i == 7:
-                rambo(num_of_sample, num_file, 'rambo', machine)
-                benign(int(num_of_sample/10), num_file, 1, machine)
-            if i == 8:
-                ramnit(num_of_sample, num_file, 'ramnit', machine)
-                benign(int(num_of_sample/10), num_file, 1, machine)
-            if i == 9:
-                banjori(num_of_sample, num_file, 'banjori', machine)
-                benign(int(num_of_sample/10), num_file, 1, machine)
-
-    elif type == 'dirichlet_client':
-        num_of_sample_10_clients = [9690,
-                                    10461,
-                                    9295,
-                                    9857,
-                                    9641,
-                                    8790,
-                                    9325,
-                                    8851,
-                                    13122,
-                                    11215]
-        for i in range (0, 10):
-            client_folder_name = f"{name_client}_{i+1}"
-            print(num_of_sample_10_clients[i])
-            generate_samples_for_clients(int(num_of_sample_10_clients[i]*10+500), num_file, dga_labels, type_for_benign, client_folder_name)
-            benign(int(num_of_sample_10_clients[i]+400), num_file, type_for_benign, client_folder_name)
-    
-    elif type == 'dirichletz_noniid':
-        pass
-            
+    gen_scripts(number_of_sample=args.n, number_file=args.num_file, dga_types=dga_labels, machine=args.name_client)
 
 
-
-
-
-
-
-    # # test
-    # print(dirichlet(10, 5, 1000))
+# python Script.py --n 100000 --num_file 1 --name_client dai --num_client 1
